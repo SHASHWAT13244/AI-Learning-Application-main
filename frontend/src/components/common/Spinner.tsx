318ce6b8ebd22923fb10e8e92e3e61965b542c5e
SHASHWAT KHANDELWAL <shashwat.khandelwal2023@ssipmt.com>
2026-04-04 14:22:06 +0530

    Update Spinner.tsx

--- frontend/src/components/common/Spinner.tsx
+++ frontend/src/components/common/Spinner.tsx
@@ -1,7 +1,7 @@
 import { BeatLoader } from 'react-spinners';
 
 const Spinner = ({ height = 'screen' }: { height?: string }) => {
-    const color = '#000';
+    const color = '#10b981';
     return (
         <div className={`flex items-center justify-center min-h-${height}`}>
             <BeatLoader size={20} color={color} />
