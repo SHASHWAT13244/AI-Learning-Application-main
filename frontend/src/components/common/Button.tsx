731ad7017731ae6935aee08d14be056fed43eb67
SHASHWAT KHANDELWAL <shashwat.khandelwal2023@ssipmt.com>
2026-04-04 14:21:33 +0530

    Update Button.tsx

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -10,13 +10,13 @@ export const Button = ({
     size = 'md',
 }: ButttonProps) => {
     const baseStyles =
-        'inline-flex items-center justify-center gap-2 font-semibold rounded-xl transiton-all duration-200 actice:scale-[0.98] disabled:opacity-50 disabled:cursor-not-allowed disabled:active:scale-100 whitespace-nowrap';
+        'inline-flex items-center justify-center gap-2 font-semibold rounded-xl transition-all duration-200 active:scale-[0.98] disabled:opacity-50 disabled:cursor-not-allowed disabled:active:scale-100 whitespace-nowrap';
     const variantStyles: Record<string, string> = {
         primary:
-            'bg-linear-to-r from-emerald-500 to-teal-500 text-white shadow-lg shadow-emerald-500/25 hover:shadow-emerald-600 hover:to-teal-600 hover:shadow-xl hover:shadow-emerald-500/30',
-        secondary: 'bg-slate-100 text-slate-700 hover:bg-slate-200',
+            'bg-linear-to-r from-emerald-500 to-teal-500 dark:from-teal-600 dark:to-blue-700 text-white shadow-lg shadow-emerald-500/25 dark:shadow-teal-500/25 hover:shadow-emerald-600 hover:to-teal-600 dark:hover:from-teal-700 dark:hover:to-blue-800 hover:shadow-xl hover:shadow-emerald-500/30 dark:hover:shadow-teal-500/30',
+        secondary: 'bg-slate-100 dark:bg-slate-800 text-slate-700 dark:text-slate-300 hover:bg-slate-200 dark:hover:bg-slate-700',
         outline:
-            'bg-white border-2 border-slate-200 text-slate-700 hover:bg-slate-50 hover:border-slate-300',
+            'bg-white dark:bg-slate-900 border-2 border-slate-200 dark:border-slate-700 text-slate-700 dark:text-slate-300 hover:bg-slate-50 dark:hover:bg-slate-800 hover:border-slate-300 dark:hover:border-slate-600',
         danger: 'bg-red-500! hover:bg-red-600 active:bg-red-700 text-white font-semibold focus:ring-red-500',
     };
     const sizeStyles: Record<NonNullable<ButttonProps['size']>, string> = {
