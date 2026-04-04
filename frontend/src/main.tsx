1e8a095d197749abe4816c119224b23810c0aaf0
SHASHWAT KHANDELWAL <shashwat.khandelwal2023@ssipmt.com>
2026-04-04 12:34:46 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,13 @@ import './index.css';
 import { Toaster } from 'react-hot-toast';
 import App from './App.tsx';
 import { AuthProvider } from './context/AuthProvider.tsx';
+import { ThemeProvider } from './context/ThemeContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
-    <AuthProvider>
-        <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
-        <App />
-    </AuthProvider>
+    <ThemeProvider>
+        <AuthProvider>
+            <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
+            <App />
+        </AuthProvider>
+    </ThemeProvider>
 );
