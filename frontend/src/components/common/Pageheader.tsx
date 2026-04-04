cf3e41c09b2e9988aec534ccef32abb6e9824060
SHASHWAT KHANDELWAL <shashwat.khandelwal2023@ssipmt.com>
2026-04-04 14:22:55 +0530

    Update Pageheader.tsx

--- frontend/src/components/common/Pageheader.tsx
+++ frontend/src/components/common/Pageheader.tsx
@@ -10,13 +10,13 @@ export const Pageheader = ({
     children?: ReactNode;
 }) => {
     return (
-        <div className="flex  items-center justify-between mb-6">
+        <div className="flex items-center justify-between mb-6">
             <div>
-                <h1 className="text-2xl font-medium text-slate-900 tracking-tight mb-2">
+                <h1 className="text-2xl font-medium text-slate-900 dark:text-slate-100 tracking-tight mb-2">
                     {title}
                 </h1>
                 {subtitle && (
-                    <p className="text-slate-400a text-sm">{subtitle}</p>
+                    <p className="text-slate-400 text-sm dark:text-slate-500">{subtitle}</p>
                 )}
             </div>
             {children && <div>{children}</div>}
