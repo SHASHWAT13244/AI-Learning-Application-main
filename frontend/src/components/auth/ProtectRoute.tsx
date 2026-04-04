b01112ab6e3fd9fa93ce19ab63816cb3705b8743
SHASHWAT KHANDELWAL <shashwat.khandelwal2023@ssipmt.com>
2026-04-04 14:44:05 +0530

    Update ProtectRoute.tsx

--- frontend/src/components/auth/ProtectRoute.tsx
+++ frontend/src/components/auth/ProtectRoute.tsx
@@ -1,12 +1,17 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { AppLayout } from '../layouts/AppLayout';
 import { useAuth } from '../../context/AuthContext';
+import Spinner from '../common/Spinner';
 
 const ProtectRoute = () => {
     const { isAuthenticated, loading } = useAuth();
 
     if (loading) {
-        return <div> Loading ...!</div>;
+        return (
+            <div className="flex items-center justify-center min-h-screen bg-white dark:bg-slate-900">
+                <Spinner />
+            </div>
+        );
     }
 
     return isAuthenticated ? (
